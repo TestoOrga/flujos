579b7d3a4b69ee368e55335aea3f220107c16972
Teniente-dan <augustogp28@gmail.com>
2021-08-10 17:44:21 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["bafar/flujos/flujos/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("bafar.flujos.flujos.controller.MainView", {});
+});
