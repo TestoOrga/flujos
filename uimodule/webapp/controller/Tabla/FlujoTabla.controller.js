de3be4c6cf19856bee35a495dbbf9158d7188564
Teniente-dan <augustogp28@gmail.com>
2021-09-02 19:24:28 -0500

    fork

--- uimodule/webapp/controller/Tabla/FlujoTabla.controller.js
+++ uimodule/webapp/controller/Tabla/FlujoTabla.controller.js
@@ -19,30 +19,42 @@ sap.ui.define(
             C7: this.formatDate(Date.now()),
           }, ];
           this.getView().setModel(new JSONModel(testModel), "tablaFlujo");
+          var oFileUploader = this.byId("Add1");
+          var that = this;
+          this._popoverDelegate = {
+            "onAfterRendering": function (oEvent) {
+              console.log('Attached to panel');
+              this.attachPopoverOnMouseover(oEvent.srcControl, this.byId("popover"));
+            }
+          }
+          this.byId("popup").addEventDelegate(this._popoverDelegate, this);
         },
         formatDate: function (oDate) {
-            if (oDate) {
-              function parse(t, a) {
-                function format(m) {
-                  let f = new Intl.DateTimeFormat("en", m);
-                  return f.format(t).padStart(2, "0");
-                }
-                return a.map(format);
+          if (oDate) {
+            function parse(t, a) {
+              function format(m) {
+                let f = new Intl.DateTimeFormat("en", m);
+                return f.format(t).padStart(2, "0");
               }
-              var a = [{
-                  year: "numeric"
-                },
-                {
-                  month: "short"
-                },
-                {
-                  day: "numeric"
-                },
-              ];
-              let s = parse(oDate, a);
-              return s[0] + " " +  s[1] + " " + s[2];
+              return a.map(format);
             }
-          },
+            var a = [{
+                year: "numeric"
+              },
+              {
+                month: "short"
+              },
+              {
+                day: "numeric"
+              },
+            ];
+            let s = parse(oDate, a);
+            return s[0] + " " + s[1] + " " + s[2];
+          }
+        },
+        onAddFile: function (oEvent) {
+
+        }
       }
     );
   }
