4d6a92902d3d82c83233331d74c3103dc9bacd54
Teniente-dan <augustogp28@gmail.com>
2021-09-07 18:40:05 -0500

    ready popover

--- uimodule/webapp/controller/Tabla/FlujoTabla.controller.js
+++ uimodule/webapp/controller/Tabla/FlujoTabla.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-  function (Controller, JSONModel) {
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+  ],
+  function (Controller,
+    JSONModel,
+    MessageToast) {
     "use strict";
 
     return Controller.extend(
@@ -17,17 +21,17 @@ sap.ui.define(
             C5: "Compensacion",
             C6: this.formatDate(Date.now()),
             C7: this.formatDate(Date.now()),
-          }, ];
+          }, {
+            C1: "000001",
+            C2: "GUZMAN AUGUSTO",
+            C3: "B2",
+            C4: this.formatDate(Date.now()),
+            C5: "Compensacion",
+            C6: this.formatDate(Date.now()),
+            C7: this.formatDate(Date.now()),
+          }];
           this.getView().setModel(new JSONModel(testModel), "tablaFlujo");
-          var oFileUploader = this.byId("Add1");
-          var that = this;
-          this._popoverDelegate = {
-            "onAfterRendering": function (oEvent) {
-              console.log('Attached to panel');
-              this.attachPopoverOnMouseover(oEvent.srcControl, this.byId("popover"));
-            }
-          }
-          // this.byId("popup").addEventDelegate(this._popoverDelegate, this);
+          console.log('finInit');
         },
         formatDate: function (oDate) {
           if (oDate) {
@@ -52,8 +56,47 @@ sap.ui.define(
             return s[0] + " " + s[1] + " " + s[2];
           }
         },
-        onAddFile: function (oEvent) {
 
+        onInputChange: function (oEvent) {
+          this.addpopover(oEvent.getSource().getParent().getAggregation("cells").find(x => x.sId.includes("colList")));
+          oEvent.getSource().getParent().removeStyleClass("lineItemSucc");
+          oEvent.getSource().getParent().setHighlight("Error");
+          oEvent.getSource().getParent().addStyleClass("lineItemError");
+          console.log();
+        },
+        addpopover: function (oControl) {
+          this._popoverDelegate = {
+            onmouseover: function (oEvent) {
+              // MessageToast.show("sadgfhsdfkgj");
+              this.attachPopoverOnMouseover(oEvent.srcControl, this.byId("popover"));
+            }
+          };
+          oControl.addEventDelegate(this._popoverDelegate, this);
+        },
+        attachPopoverOnMouseover: function (targetControl, popover) {
+          targetControl.addEventDelegate({
+            onmouseover: this._showPopover.bind(this, targetControl, popover),
+            onmouseout: this._clearPopover.bind(this, popover),
+          }, this);
+        },
+        _showPopover: function (targetControl, popover) {
+          // var oCarpeta = this.getView().getModel("detailModel").getProperty(targetControl.getBindingContext("detailModel").sPath);
+          // this.byId("infoPopUpText").setText(oCarpeta.Ayuda);
+          var testo = "asdfasdfasdf";
+          this.byId("infoPopUpText").setText(testo);
+          this.byId("popover").setTitle(testo);
+          this._timeId = setTimeout(() => popover.openBy(targetControl), 500);
+        },
+        _clearPopover: function (popover) {
+          clearTimeout(this._timeId) || popover.close();
+        },
+
+        onAddLine: function (oEvent) {
+
+        },
+
+        onItemPress: function (oEvent) {
+          // this._showPopover(oEvent.getSource(), this.byId("popover"));
         }
       }
     );
