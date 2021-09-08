c7fd902be12a2e38b74d61ddd4ed1fd37b8d30df
Teniente-dan <augustogp28@gmail.com>
2021-09-08 09:42:27 -0500

    flow control ready

--- uimodule/webapp/controller/HeaderFlujos.controller.js
+++ uimodule/webapp/controller/HeaderFlujos.controller.js
@@ -47,18 +47,6 @@ sap.ui.define(
             console.log("display");
           }
         }, this);
-        var oFlujo = new JSONModel({
-          seccion1: this.get18().getText(
-            "Pensiones.DatosPersonales.panel"
-          ),
-          seccion2: this.get18().getText(
-            "Pensiones.Juridico.panel"
-          ),
-          seccion3: this.get18().getText(
-            "Pensiones.Deudas.panel"
-          ),
-        });
-        this.getView().setModel(oFlujo, "flujo");
       },
 
       getDepartamento: function () {
@@ -99,7 +87,7 @@ sap.ui.define(
         console.log("BeforeRendering");
 
       },
-      /**          this.mainModel = this.getModel();
+      /**          
 
        * @override
        */
@@ -112,14 +100,47 @@ sap.ui.define(
       },
       onAddFlow: function (oEvent) {
         console.log("Event handler: onAddFlow");
-        if (this.headerData.proceso === "001") {
-          this.addSpecFlow();
+        var flowConfig = this.getModel("flowConfig").getData();
+        if (this.headerData.proceso === "001") {                 
+          this.addSpecFlow(flowConfig.find(x=>x.pensiones).pensiones);
         } else {
-          this.addSpecFlow1();
+          this.addSpecFlow(flowConfig.find(x=>x.sueldos).sueldos);
         }
         this.byId("headerFlujosButNuevo").setEnabled(false);
       },
-      addSpecFlow1: function () {
+      // addSpecFlow1: function () {
+      //   // if (!this.valHeaderInput()) {
+      //   //   MessageBox.error(this.get18().getText("createFlowError"));
+      //   //   return
+      //   // }
+      //   var oPayload = {
+      //     P1: "CAT",
+      //     P2: "ID",
+      //     to_pesal: []
+      //   };
+      //   this.getCatData(oPayload).then((res) => {
+      //     if (res.length > 0) {
+      //       var id = res[0].C1;
+      //       this.headerData.id = id;
+      //       this.byId("headerFLujosIdFlujo").setText(id);
+      //     }
+      //   });
+      //   console.log("Event Handler: onAddFlow");
+      //   this.views = [{
+      //     controlId: "headerFlujosInsertPanel1",
+      //     controllerName: "bafar.flujos.flujos.controller.Tabla.FlujoTabla",
+      //     viewId: "flujostabla",
+      //     viewName: "bafar.flujos.flujos.view.Tabla.FlujoTabla"
+      //   }];
+      //   this.views.forEach((view) => {
+      //     var oView = this.specificFlow(view.controlId, view.controllerName, view.viewId, view.viewName);
+      //     console.log(view.viewId);
+      //     oView.then((res) => {
+      //       res.oView.placeAt(res.oRef).addStyleClass("headerPanel").addStyleClass("FlexContent");
+      //     })
+      //   });
+      // },
+      addSpecFlow: async function (flowConfig) {
         // if (!this.valHeaderInput()) {
         //   MessageBox.error(this.get18().getText("createFlowError"));
         //   return
@@ -137,75 +158,36 @@ sap.ui.define(
           }
         });
         console.log("Event Handler: onAddFlow");
-        this.views = [{
-          controlId: "headerFlujosInsertPanel1",
-          controllerName: "bafar.flujos.flujos.controller.Tabla.FlujoTabla",
-          viewId: "flujostabla",
-          viewName: "bafar.flujos.flujos.view.Tabla.FlujoTabla"
-        }];
-        // var time = 0;
-        this.views.forEach((view) => {
-          // setTimeout(() => {
-          var oView = this.specificFlow(view.controlId, view.controllerName, view.viewId, view.viewName);
-          console.log(view.viewId);
-          oView.then((res) => {
-            res.oView.placeAt(res.oRef).addStyleClass("headerPanel").addStyleClass("FlexContent");
-          })
-          // }, time);
-          // time = time + 1000;
-        });
-      },
-      addSpecFlow: async function () {
-        if (!this.valHeaderInput()) {
-          MessageBox.error(this.get18().getText("createFlowError"));
-          return
-        }
-        var oPayload = {
-          P1: "CAT",
-          P2: "ID",
-          to_pesal: []
-        };
-        this.getCatData(oPayload).then((res) => {
-          if (res.length > 0) {
-            var id = res[0].C1;
-            this.headerData.id = id;
-            this.byId("headerFLujosIdFlujo").setText(id);
-          }
-        });
-        console.log("Event Handler: onAddFlow");
-        this.views = [{
-            controlId: "headerFlujosInsertPanel1",
-            controllerName: "bafar.flujos.flujos.controller.PensionesC.DatosPersonales",
-            viewId: "PensionesDatosPersonales",
-            viewName: "bafar.flujos.flujos.view.PensionesV.DatosPersonales"
-          },
-          {
-            controlId: "headerFlujosInsertPanel1",
-            controllerName: "bafar.flujos.flujos.controller.PensionesC.JuridicaDeudas",
-            viewId: "PensionesDatosJuridica",
-            viewName: "bafar.flujos.flujos.view.PensionesV.Juridica"
-          },
-          {
-            controlId: "headerFlujosInsertPanel1",
-            controllerName: "bafar.flujos.flujos.controller.PensionesC.JuridicaDeudas",
-            viewId: "PensionesDatosDeudas",
-            viewName: "bafar.flujos.flujos.view.PensionesV.Deudas"
-          },
-          {
-            controlId: "headerFlujosInsertPanel1",
-            controllerName: "bafar.flujos.flujos.controller.Comunes.ArchivosExtra",
-            viewId: "ArchivosExtra",
-            viewName: "bafar.flujos.flujos.view.Comunes.ArchivosExtra"
-          }
-        ];
-        // var time = 0;
+        this.views = flowConfig;
+        // [{
+        //     controlId: "headerFlujosInsertPanel1",
+        //     controllerName: "bafar.flujos.flujos.controller.PensionesC.DatosPersonales",
+        //     viewId: "PensionesDatosPersonales",
+        //     viewName: "bafar.flujos.flujos.view.PensionesV.DatosPersonales"
+        //   },
+        //   {
+        //     controlId: "headerFlujosInsertPanel1",
+        //     controllerName: "bafar.flujos.flujos.controller.PensionesC.JuridicaDeudas",
+        //     viewId: "PensionesDatosJuridica",
+        //     viewName: "bafar.flujos.flujos.view.PensionesV.Juridica"
+        //   },
+        //   {
+        //     controlId: "headerFlujosInsertPanel1",
+        //     controllerName: "bafar.flujos.flujos.controller.PensionesC.JuridicaDeudas",
+        //     viewId: "PensionesDatosDeudas",
+        //     viewName: "bafar.flujos.flujos.view.PensionesV.Deudas"
+        //   },
+        //   {
+        //     controlId: "headerFlujosInsertPanel1",
+        //     controllerName: "bafar.flujos.flujos.controller.Comunes.ArchivosExtra",
+        //     viewId: "ArchivosExtra",
+        //     viewName: "bafar.flujos.flujos.view.Comunes.ArchivosExtra"
+        //   }
+        // ];        
         var viewArr = [];
-        this.views.forEach((view) => {
-          //   setTimeout(() => {
+        this.views.forEach((view) => {          
           viewArr.push(this.specificFlow(view.controlId, view.controllerName, view.viewId, view.viewName));
-          console.log(view.viewId);
-          // }, time);
-          // time = time + 1000;
+          console.log(view.viewId);          
         });
         let values = await Promise.all(viewArr);
         values.forEach((element) => {
@@ -239,19 +221,22 @@ sap.ui.define(
         }
         this.byId("headerFlujosButNuevo").setEnabled(true);
       },
-      onConfirmDialogPress: function (fn, text) {
+      onConfirmDialogPress: function (fn, text, risk) {
         this.oApproveDialog = new Dialog({
           type: DialogType.Message,
           icon: this.get18().getText("submitIcon"),
           title: this.get18().getText("submitConfirmation"),
           state: "Warning",
+          afterClose: function () {
+            this.oApproveDialog.destroy();
+          }.bind(this),
           content: new Text({
             text: text
           }),
           beginButton: new Button({
             // type: ButtonType.Emphasized,
             icon: this.get18().getText("submitIconAccept"),
-            // type: sap.m.ButtonType.Accept,
+            type: risk ? sap.m.ButtonType.Accept : sap.m.ButtonType.Transparent,
             press: function () {
               fn();
               this.oApproveDialog.close();
@@ -259,7 +244,7 @@ sap.ui.define(
           }),
           endButton: new Button({
             icon: this.get18().getText("submitIconCancel"),
-            // type: sap.m.ButtonType.Reject,
+            type: risk ? sap.m.ButtonType.Reject : sap.m.ButtonType.Transparent,
             press: function () {
               this.oApproveDialog.close();
             }.bind(this)
@@ -275,19 +260,14 @@ sap.ui.define(
           oControl.setSelectedKey("");
         });
       },
+      
+      // Crea vistas de acuerdo al flujo seleccionado
       specificFlow: async function (controlId, controllerName, viewId, viewName) {
         var oRef = this.getView().byId(controlId);
         var oController = sap.ui.core.mvc.Controller.create({
           name: controllerName
         });
 
-        // XMLView.create({
-        //   id: this.createId(viewId),
-        //   viewName: viewName
-        // }).then(function (oView) {
-        //   // the instance is available in the callback function
-        //   oView.placeAt(oRef).addStyleClass("headerPanel").addStyleClass("FlexContent");
-        // }.bind(this))               
         const fn = () => XMLView.create({
           id: this.createId(viewId),
           viewName: viewName
@@ -297,41 +277,10 @@ sap.ui.define(
           oView: oView,
           oRef: oRef
         };
-        // oView.placeAt(oRef).addStyleClass("headerPanel").addStyleClass("FlexContent");
 
       },
       onGrabar: function () {
-        this.onApproveDialogPress();
-      },
-      onApproveDialogPress: function () {
-        if (!this.oApproveDialog) {
-          this.oApproveDialog = new Dialog({
-            type: DialogType.Message,
-            icon: this.get18().getText("submitIcon"),
-            title: this.get18().getText("submitConfirmation"),
-            state: "Warning",
-            content: new Text({
-              text: this.get18().getText("submitConfirmationQuestion")
-            }),
-            beginButton: new Button({
-              type: ButtonType.Emphasized,
-              icon: this.get18().getText("submitIconAccept"),
-              type: sap.m.ButtonType.Accept,
-              press: function () {
-                this.submitFlow();
-                this.oApproveDialog.close();
-              }.bind(this)
-            }),
-            endButton: new Button({
-              icon: this.get18().getText("submitIconCancel"),
-              type: sap.m.ButtonType.Negative,
-              press: function () {
-                this.oApproveDialog.close();
-              }.bind(this)
-            })
-          });
-        }
-        this.oApproveDialog.open();
+        this.onConfirmDialogPress(this.submitFlow.bind(this), this.get18().getText("submitConfirmationQuestion"), true)        
       },
 
       submitFlow: function () {
