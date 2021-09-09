8f23ff506157fd002609aedff73b2d359db14970
Diego <diego.rodriguez@nubaj.com>
2021-09-09 03:26:36 -0500

    Save and Validate fields

--- uimodule/webapp/controller/PensionesC/JuridicaDeudas.controller.js
+++ uimodule/webapp/controller/PensionesC/JuridicaDeudas.controller.js
@@ -14,18 +14,29 @@ sap.ui.define(
       oODataJSONModel,
       sServiceUrl,
       oDataResults,
-      oEntityUnidadIntervalo;
+      numeroOrdenDate,
+      oEntityUnidadIntervalo,
+      claveBancoKey,
+      claveBancoText,
+      juridicaDeudasModel;
+
+    var juridicaDeudasData = {
+      ClaveBanco: "",
+      vViaPago: "",
+      Receptor: "",
+      CuentaBancaria: "",
+      NumeroOrden: "",
+      TipoDescuento: "",
+      UnidadIntervalo: ""
+    }
 
     return Controller.extend("namespace.name.project3.controller.View1", {
       onInit: function () {
-        // Step 1
 
         oDataResults = [];
         oODataJSONModel = new sap.ui.model.json.JSONModel();
         sServiceUrl = "/sap/opu/odata/sap/ZOD_FLUJOS_SRV";
         oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
-
-        // Step 2
 
         var oEntityClaveBanco = {
           P1: "CAT",
@@ -34,9 +45,9 @@ sap.ui.define(
         };
 
         /*var oEntity = {
-					ClaveBanco : { P1 : "CAT", P2 : "BANKL"},
-					ViaPago : { P1 : "CAT", P2 : "VIA"}
-				};*/
+                    ClaveBanco : { P1 : "CAT", P2 : "BANKL"},
+                    ViaPago : { P1 : "CAT", P2 : "VIA"}
+                };*/
 
         var oEntityViaPago = {
           P1: "CAT",
@@ -57,26 +68,25 @@ sap.ui.define(
           to_pesal: [],
         };
 
-        // Step 3
 
         /*Model.create("/BaseSet", oEntityViaPago, {
 
-					success: function (oData, oResponse) {
-						sap.m.MessageToast.show(" Creation success");
-						oODataJSONModel.setData({
-							viaPago: oData.to_pesal.results
-						});
-						
-						//window.console.log(oDataResults)
-					},
+                    success: function (oData, oResponse) {
+                        sap.m.MessageToast.show(" Creation success");
+                        oODataJSONModel.setData({
+                            viaPago: oData.to_pesal.results
+                        });
+                        
+                        //window.console.log(oDataResults)
+                    },
 
-					error: function (oError) {
-						// Error
-						sap.m.MessageToast.show(" Creation failed");
-					}
-				});*/
+                    error: function (oError) {
+                        // Error
+                        sap.m.MessageToast.show(" Creation failed");
+                    }
+                });*/
 
-        var that = this;
+
         oModel.create("/BaseSet", oEntityClaveBanco, {
           success: function (oData, oResponse) {
             // Success
@@ -133,6 +143,14 @@ sap.ui.define(
 
         this.getView().setModel(oODataJSONModel);
         window.console.log(oODataJSONModel);
+
+        //Funcionalidad fecha actual start
+        this.getDate();
+        //Funcionalidad fecha actual end
+
+        //
+
+        //
       },
       onButAction: function (oEvent, param) {
         MessageToast.show(param);
@@ -144,11 +162,9 @@ sap.ui.define(
           var descuentoKey = selectedItem.getKey();
           var descuentoText = selectedItem.getText();
           this.getView().byId("_tipoDescuentoInput").setValue(descuentoKey);
-          oEntityUnidadIntervalo.P3 = descuentoText;
+          oEntityUnidadIntervalo.P3 = descuentoKey;
           this.getView().byId("_unidadIntervalo").setEnabled(true);
-          this.getView()
-            .byId("_unidadIntervaloLabel")
-            .setText("Unidad Intervalo");
+          this.getView().byId("_unidadIntervaloLabel").setText("Unidad Intervalo");
         } else {
           this.getView().byId("_unidadIntervalo").setEnabled(false);
         }
@@ -170,10 +186,18 @@ sap.ui.define(
           },
         });
         oSelectUnidadIntervalo.setModel(oODataJSONModel);
+
+        var descuentoSplit = descuentoText.split(' ')[1];
+        this.getView().byId("descuentoCurrLabel").setVisible(true);
+        this.getView().byId("descuentoCurrLabel").setText(descuentoSplit);
+        this.getView().byId("descuentoCurr").setVisible(true);
+
+
       },
       onSelectClaveBanco: function (oEvent) {
         var selectedItem = oEvent.getParameter("selectedItem");
-        var claveBancoKey = selectedItem.getKey();
+        claveBancoKey = selectedItem.getKey();
+        claveBancoText = selectedItem.getText();
         this.getView().byId("_claveBancoInput").setValue(claveBancoKey);
       },
       onSelectViaPago: function (oEvent) {
@@ -184,10 +208,126 @@ sap.ui.define(
       onSelectUnidadIntervalo: function (oEvent) {
         var selectedItem = oEvent.getParameter("selectedItem");
         var unidadIntervaloKey = selectedItem.getKey();
-        this.getView()
-          .byId("_unidadIntervaloInput")
-          .setValue(unidadIntervaloKey);
+        this.getView().byId("_unidadIntervaloInput").setValue(unidadIntervaloKey);
       },
+      //Funcionalidad fecha actual 
+      getDate: function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+        var hour = today.getHours();
+        var Minutes = today.getMinutes();
+        var Seconds = today.getSeconds();
+        var today1 = (dd + '.' + mm + '.' + yyyy);
+        numeroOrdenDate = this.getView().byId("numeroOrdenDate");
+        if (typeof (numeroOrdenDate) !== "undefined") {
+          numeroOrdenDate.setValue(today1);
+        }
+      },
+      //Funcionalidad fecha actual
+
+      //Validate Fields Juridica Deudas
+      validateFieldsJuridicaDeudas: function () {
+
+        var juridicaDeudasFields = [
+          "cveBancoSelect",
+          "_viaPagoSelect",
+          "receptor",
+          "cuentaBancariaInput",
+          "numeroOrdenInput",
+          "_tipoDescuento",
+          "_unidadIntervalo",
+
+        ];
+        var error = false;
+        for (var i = 0; i < juridicaDeudasFields.length; i++) {
+          var value = "";
+          if (this.getView().byId(juridicaDeudasFields[i])) {
+            try {
+              value = this.getView().byId(juridicaDeudasFields[i]).getValue();
+            } catch (oError) {
+              try {
+                value = this.getView().byId(juridicaDeudasFields[i]).getSelectedKey();
+              } catch (oError) {
+                value = this.getView().byId(juridicaDeudasFields[i]).getSelected();
+              }
+            }
+
+            if (value) {
+              value = parseFloat(value);
+            }
+            if (value === "" || value === false || value === 0) {
+              this.getView().byId(juridicaDeudasFields[i]).setValueState("Error");
+              var error = true;
+            } else {
+              this.getView().byId(juridicaDeudasFields[i]).setValueState("None");
+            }
+          }
+        }
+        if (error) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+      //Validate Fields JuridicaDeudas
+
+      //Guardado JuridicaDeudas
+      saveJuridicaDeudasData: function () {
+
+        var claveBancoSplit = claveBancoText.split(' ')[0];
+        juridicaDeudasData = {
+          ClaveBanco: claveBancoSplit,
+          ViaPago: this.getView().byId("_viaPagoSelect").getSelectedKey(),
+          Receptor: this.getView().byId("receptor").getValue(),
+          CuentaBancaria: this.getView().byId("cuentaBancariaInput").getValue(),
+          NumeroOrden: this.getView().byId("numeroOrdenInput").getValue(),
+          TipoDescuento: this.getView().byId("_tipoDescuento").getSelectedKey(),
+          UnidadIntervalo: this.getView().byId("_unidadIntervalo").getSelectedKey()
+        }
+        //GuardadoJuridicaDeudas
+
+
+        /*juridicaDeudasData = {
+          ClaveBanco: "test",
+          vViaPago: "test",
+          Receptor: "test",
+          CuentaBancaria: "test",
+          NumeroOrden: "test",
+          TipoDescuento: "test",
+          UnidadIntervalo: "test"
+      }*/
+
+
+        /*juridicaDeudasModel = new sap.ui.model.json.JSONModel();
+        juridicaDeudasModel.setData(juridicaDeudasData);*/
+      },
+
+
+      //Descuento Currency field format
+      formatCurrency: function () {
+        var formatter = new Intl.NumberFormat('en-us', {
+          style: 'currency',
+          currency: 'USD'
+        });
+        var currency = this.getView().byId("descuentoCurr").getValue();
+        var currencyFormated = formatter.format(currency);
+        this.getView().byId("descuentoCurr").setValue(currencyFormated);
+
+        /*var currency = this.getView().byId("descuentoCurr").getValue();
+        var currencyFormated = currency + ".00"
+        this.getView().byId("descuentoCurr").setValue(currencyFormated);*/
+      },
+      //Descuento Currency field Format
+
+      validateNumericValues: function (oEvent) {
+        var inputCurrency = oEvent.getSource();
+        var val = inputCurrency.getValue();
+        val = val.replace(/[^\d]/g, '');
+        inputCurrency.setValue(val);
+      }
     });
   }
 );
