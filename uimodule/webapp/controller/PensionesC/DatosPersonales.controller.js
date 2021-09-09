e5a683602b10298d1b7c634820f92761928749bc
Oscar <oscar.rdgz.ram@gmail.com>
2021-09-08 22:12:46 -0500

    Validacion de datos personales y nuevo Odata

--- uimodule/webapp/controller/PensionesC/DatosPersonales.controller.js
+++ uimodule/webapp/controller/PensionesC/DatosPersonales.controller.js
@@ -8,140 +8,18 @@ sap.ui.define(
     "use strict";
 
     var noPersonalIn;
+    var fechIniRet;
 
     return Controller.extend(
-      "bafar.flujos.flujos.controller.PensionesC.DatosPersonales", {
+      "bafar.flujos.flujos.controller.PensionesC.DatosPersonales",
+      {
         onInit: function () {
           window.console.log("Se inicia onInit");
-          // Test
-          /*
-        var oData = {
-        	"SelectedProduct": "HT-1001",
-        	"SelectedProduct2": "HT-1001",
-        	"SelectedProduct3": "HT-1001",
-        	"ProductCollection": [
-        		{
-        			"ProductId": "HT-1000",
-        			"Name": "Notebook Basic 15"
-        		},
-        		{
-        			"ProductId": "HT-1001",
-        			"Name": "Notebook Basic 17"
-        		},
-        		{
-        			"ProductId": "HT-1002",
-        			"Name": "Notebook Basic 18"
-        		},
-        		{
-        			"ProductId": "HT-1003",
-        			"Name": "Notebook Basic 19"
-        		},
-        		{
-        			"ProductId": "HT-1007",
-        			"Name": "ITelO Vault"
-        		}
-        	],
-        	"ProductCollection2": [
-        		{
-        			"ProductId": "HT-1000",
-        			"Name": "Notebook Basic 15"
-        		},
-        		{
-        			"ProductId": "HT-1001",
-        			"Name": "Notebook Basic 17"
-        		},
-        		{
-        			"ProductId": "HT-1002",
-        			"Name": "Notebook Basic 18"
-        		},
-        		{
-        			"ProductId": "HT-1003",
-        			"Name": "Notebook Basic 19"
-        		},
-        		{
-        			"ProductId": "HT-1007",
-        			"Name": "ITelO Vault"
-        		}
-        	],
-        	"ProductCollection3": [
-        		{
-        			"ProductId": "HT-1000",
-        			"Name": "Notebook Basic 15"
-        		},
-        		{
-        			"ProductId": "HT-1001",
-        			"Name": "Notebook Basic 17"
-        		},
-        		{
-        			"ProductId": "HT-1002",
-        			"Name": "Notebook Basic 18"
-        		},
-        		{
-        			"ProductId": "HT-1003",
-        			"Name": "Notebook Basic 19"
-        		},
-        		{
-        			"ProductId": "HT-1007",
-        			"Name": "ITelO Vault"
-        		}
-        	],
-        	"Editable": true,
-        	"Enabled": true
-        };
-        
-        var jsonTestModel = new sap.ui.model.json.JSONModel();
-        jsonTestModel.setData(oData);
-        this.getView().setModel(jsonTestModel);
-        window.console.log(jsonTestModel);
-        */
-          /*
-				// Step 1
-				
-				var oDataResults = [];
-				var oODataJSONModel = new sap.ui.model.json.JSONModel();
-				var sServiceUrl = "/sap/opu/odata/sap/ZOD_FLUJOS_SRV";
-				var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
-				
-				// Step 2
-
-				var oEntityData = {
-					P1: "CAT",
-					P2: "PERNR",
-					P3: "00000001",
-
-					to_pesal: []
-				};
-
-				// Step 3
-
-				oModel.create("/BaseSet", oEntityData, {
-					
-					success : function(oData, oResponse) {
-						 // Success
-						 sap.m.MessageToast.show(" Created Successfully" );
-						 //window.console.log(oData.to_pesal.results);
-						 oODataJSONModel.setData({
-							catalogo: oData.to_pesal.results
-						});
-						oDataResults = oData.to_pesal.results;
-					},
-
-					error : function(oError) {
-						 // Error
-					   sap.m.MessageToast.show(" Creation failed" );
-					}
-			  	});
-
-				this.getView().setModel(oODataJSONModel);
-				window.console.log(oODataJSONModel);
-				window.console.log(oDataResults);
-				*/
         },
 
         onEnterInputNoPersonal: function (oEvent) {
           noPersonalIn = this.getView().byId("noPeronsal_Input").getValue();
           noPersonalIn.toString();
-          //window.console.log(noPersonalIn);
 
           // Step 1
 
@@ -165,8 +43,7 @@ sap.ui.define(
           oModel.create("/BaseSet", oEntityData, {
             success: function (oData, oResponse) {
               // Success
-              sap.m.MessageToast.show(" Created Successfully");
-              //window.console.log(oData.to_pesal.results);
+              //sap.m.MessageToast.show(" Created Successfully");
               oODataJSONModel.setData({
                 catalogo: oData.to_pesal.results,
               });
@@ -175,15 +52,14 @@ sap.ui.define(
 
             error: function (oError) {
               // Error
-              sap.m.MessageToast.show(" Creation failed");
+              //sap.m.MessageToast.show(" Creation failed");
             },
           });
 
           this.getView().setModel(oODataJSONModel);
-          window.console.log(oODataJSONModel);
-          window.console.log(oDataResults);
 
           // Setear valores a inputs
+
           this.getView().byId("noPeronsal_Input1").setValue(oDataResults[0].C2);
           this.getView().byId("sociedad_Input").setValue(oDataResults[0].C3);
           this.getView().byId("sociedad_Input1").setValue(oDataResults[0].C16);
@@ -192,9 +68,147 @@ sap.ui.define(
           this.getView().byId("funcion_Input").setValue(oDataResults[0].C10);
           this.getView().byId("funcion_Input1").setValue(oDataResults[0].C14);
           this.getView().byId("areaNomina_Input").setValue(oDataResults[0].C12);
+
+          // Habilitar campo de Periodo de retencion
+
+          if (oDataResults.length > 0) {
+            this.getView().byId("periodoRet_Input").setEnabled(true);
+            this.getView().byId("periodoRet_Input1").setEnabled(true);
+          } else {
+            sap.m.MessageToast.show("No existen registros");
+          }
         },
-        test: function () {
-          console.log();
+
+        onEnterFechaRet: function () {
+          var areaNom = this.getView().byId("areaNomina_Input").getValue();
+          var perRet1 = this.getView().byId("periodoRet_Input").getValue();
+          var perRet2 = this.getView().byId("periodoRet_Input1").getValue();
+
+          if (areaNom.length > 0 && perRet1.length > 0 && perRet2.length > 0) {
+            var oEntityData2 = {
+              P1: "CAT",
+              P2: "PERIODO",
+              P3: areaNom,
+              P4: perRet1,
+              P5: perRet2,
+
+              to_pesal: [],
+            };
+
+            var oDataResults2 = [];
+            var oODataJSONModel2 = new sap.ui.model.json.JSONModel();
+            var sServiceUrl = "/sap/opu/odata/sap/ZOD_FLUJOS_SRV";
+            var oModel2 = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+
+            oModel2.create("/BaseSet", oEntityData2, {
+              success: function (oData, oResponse) {
+                // Success
+                //sap.m.MessageToast.show(" Created Successfully");
+
+                oODataJSONModel2.setData({
+                  catalogo: oData.to_pesal.results,
+                });
+                oDataResults2 = oData.to_pesal.results;
+              },
+
+              error: function (oError) {
+                // Error
+                //sap.m.MessageToast.show(" Creation failed");
+              },
+            });
+
+            if (oDataResults2.length === 0) {
+              sap.m.MessageToast.show("No existen registros");
+            }
+
+            // Seteat campo Fecha Ini Ret
+            fechIniRet = oDataResults2[0].C1;
+            this.getView().byId("fechaIniRet_Input").setValue(fechIniRet);
+          } else {
+            sap.m.MessageToast.show(
+              "Favor de validar los campos Área Nómina / Periodo de retención"
+            );
+          }
+        },
+
+        onGetDataFromInput: function () {
+          var datosPersonalesFields = [
+            "noPeronsal_Input",
+            "noPeronsal_Input1",
+            "sociedad_Input",
+            "sociedad_Input1",
+            "division_Input",
+            "division_Input1",
+            "funcion_Input",
+            "funcion_Input1",
+            "areaNomina_Input",
+            "periodoRet_Input",
+            "periodoRet_Input1",
+            "fechaIniRet_Input",
+          ];
+
+          var datosPersonalesData = [];
+
+          for (var i = 0; i < datosPersonalesFields.length; i++) {
+            datosPersonalesData.push(
+              this.getView().byId(datosPersonalesFields[i]).getValue()
+            );
+          }
+
+          //console.log(datosPersonalesData);
+        },
+
+        onValidateInputs: function () {
+          var datosPersonalesFields = [
+            "noPeronsal_Input",
+            "noPeronsal_Input1",
+            "sociedad_Input",
+            "sociedad_Input1",
+            "division_Input",
+            "division_Input1",
+            "funcion_Input",
+            "funcion_Input1",
+            "areaNomina_Input",
+            "periodoRet_Input",
+            "periodoRet_Input1",
+            "fechaIniRet_Input",
+          ];
+
+          var error = false;
+          for (var i = 0; i < datosPersonalesFields.length; i++) {
+            var value = "";
+            if (this.getView().byId(datosPersonalesFields[i])) {
+              try {
+                value = this.getView()
+                  .byId(datosPersonalesFields[i])
+                  .getValue();
+              } catch (oError) {
+                try {
+                  value = this.getView()
+                    .byId(datosPersonalesFields[i])
+                    .getSelectedKey();
+                } catch (oError) {
+                  value = this.getView()
+                    .byId(datosPersonalesFields[i])
+                    .getSelected();
+                }
+              }
+
+              if (value) {
+                value = parseFloat(value);
+              }
+              if (value === "" || value === false || value === 0) {
+                this.getView()
+                  .byId(datosPersonalesFields[i])
+                  .setValueState("Error");
+                var error = true;
+              } else {
+                this.getView()
+                  .byId(datosPersonalesFields[i])
+                  .setValueState("None");
+              }
+            }
+          }
         },
         /**
          * @override
@@ -206,7 +220,7 @@ sap.ui.define(
          * @override
          */
         onButAction: function () {
-          MessageToast.show("Datos Personales");
+          //MessageToast.show("Datos Personales");
         },
       }
     );
